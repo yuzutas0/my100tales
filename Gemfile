a941818968cf68c4b8cff8f79ec730c708eb09c5
yuzutas0 <sho.y0921@gmail.com>
2016-05-03 21:40:56 +0900

    connect docker redis

--- Gemfile
+++ Gemfile
@@ -3,6 +3,7 @@ source 'https://rubygems.org'
 
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '4.2.6'
+
 # Use SCSS for stylesheets
 gem 'sass-rails', '~> 5.0'
 # Use Uglifier as compressor for JavaScript assets
@@ -21,9 +22,6 @@ gem 'jbuilder', '~> 2.0'
 # bundle exec rake doc:rails generates the API under doc/api.
 gem 'sdoc', '~> 0.4.0', group: :doc
 
-# Use ActiveModel has_secure_password
-# gem 'bcrypt', '~> 3.1.7'
-
 # Use Unicorn as the app server
 # gem 'unicorn'
 
@@ -31,7 +29,10 @@ gem 'sdoc', '~> 0.4.0', group: :doc
 # gem 'capistrano-rails', group: :development
 
 # custom
+# M/W
 gem 'mysql2'
+gem 'redis-rails'
+# AP
 gem 'devise'
 
 group :development, :test do
--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,45 @@
 # Be sure to restart your server when you modify this file.
 
-Rails.application.config.session_store :cookie_store, key: '_my100tales_session'
+# manage session by rails cookie
+def use_default(message)
+  Rails.logger.error(message)
+  Rails.application.config.session_store :cookie_store, key: '_my100tales_session'
+end
+
+# manage session by redis
+def use_redis(config)
+  Rails.application.config.session_store :redis_store, servers: {
+      # password: config[:password],
+      host: config[:host],
+      port: config[:port],
+      db: config[:db],
+      namespace: config[:namespace],
+  }, expire_after: eval(config[:expire_after])
+end
+
+# util for redis setting
+def yml_include_erb(params)
+  results = {}
+  params.each { |key, value|
+    results[key] = ERB.new(value.to_s).result(binding)
+  }
+  return results
+end
+
+# main
+config_file = File.join(Rails.root, 'config', 'redis_store.yml')
+if File.exists?(config_file)
+  begin
+    config = YAML.load_file(config_file)[Rails.env].symbolize_keys
+    config = yml_include_erb(config)
+    begin
+      use_redis(config)
+    rescue
+      use_default('connection to redis is failed!')
+    end
+  rescue
+    use_default("#{config_file} is missing!")
+  end
+else
+  use_default("#{config_file} is missing!")
+end
