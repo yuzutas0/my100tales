82f196cdf043db648bd61e88f72c175af97c09c3
yuzutas0 <sho.y0921@gmail.com>
2016-05-08 16:31:22 +0900

    fix layout about container, form, buttom

--- app/helpers/tales_helper.rb
+++ app/helpers/tales_helper.rb
@@ -2,7 +2,9 @@
 # TalesHelper
 #
 module TalesHelper
-  def markdown(text)
+
+  # render from markdown text to html
+  def markdown_to_html(text)
     # set param
     extentions = { tables: true, fenced_code_blocks: true }
     render_options = { filter_html: true, hard_wrap: true }
@@ -10,5 +12,10 @@ module TalesHelper
     # execute rendering
     renderer = Redcarpet::Render::HTML.new(render_options)
     Redcarpet::Markdown.new(renderer, extentions).render(text).html_safe
+  end
+
+  # render from markdown text to plain text
+  def markdown_to_plain(text)
+    strip_tags(simple_format(h(markdown_to_html(text))))
   end
 end
---
+++ config/initializers/constants.rb
@@ -0,0 +1,8 @@
+# manage constant value
+module Constants
+  # routes name to show user
+  HOME_INDEX_PATH_NAME = 'Top'
+  TALES_PATH_NAME= 'MyPage'
+  NEW_TALE_PATH_NAME = 'Create New Tale'
+  EDIT_TALE_PATH_NAME = 'Edit Tale'
+end
