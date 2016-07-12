0ee805d61a930d071b05e4ba0947fea08e801ff2
yuzutas0 <sho.y0921@gmail.com>
2016-07-12 21:54:20 +0900

    remove useless code from sequels

--- app/controllers/sequels_controller.rb
+++ app/controllers/sequels_controller.rb
@@ -8,12 +8,10 @@ class SequelsController < ApplicationController
     Sequel.transaction do
       @tale = Tale.detail(params[:view_number], current_user)
       sequel = @tale.sequels.build(sequel_params)
-      puts "1111111111"
       puts sequel.attributes
       if sequel.save
         redirect_to @tale, notice: 'Sequel was successfully created.'
       else
-        raise
         redirect_to @tale, alert: 'Error - sequel was not created!'
       end
     end
