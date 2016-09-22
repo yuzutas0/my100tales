9b5b3fb03ba4d01ce4d77dca1ae459177a57f9c5
yuzutas0 <sho.y0921@gmail.com>
2016-09-22 19:20:34 +0900

    refactor

--- app/repositories/tale_repository.rb
+++ app/repositories/tale_repository.rb
@@ -11,31 +11,21 @@ class TaleRepository
 
   # get index by MariaDB
   def self.list(user_id, page)
-    Tale.where('user_id = ?', user_id)
-        .page(page)
-        .per(RECORD_LIST_LIMIT_SIZE)
-        .order(created_at: :desc)
-        .includes(:tale_tag_relationships)
+    condition = Tale.where('user_id = ?', user_id)
+    read_index(condition, page)
   end
 
   # search by MariaDB
   def self.search_by_db(user_id, keyword, page)
     keyword = '%' + keyword + '%'
-    Tale.where('user_id = ? AND (title LIKE ? OR content LIKE ?)', user_id, keyword, keyword)
-        .page(page)
-        .per(RECORD_LIST_LIMIT_SIZE)
-        .order(created_at: :desc)
-        .includes(:tale_tag_relationships)
+    condition = Tale.where('user_id = ? AND (title LIKE ? OR content LIKE ?)', user_id, keyword, keyword)
+    read_index(condition, page)
   end
 
   # search by Elasticsearch
   def self.search_by_es(user_id, query, page)
-    Tale.search_index(user_id, query)
-        .records
-        .page(page)
-        .per(RECORD_LIST_LIMIT_SIZE)
-        .order(created_at: :desc)
-        .includes(:tale_tag_relationships)
+    condition = Tale.search_index(user_id, query).records
+    read_index(condition, page)
   end
 
   # -----------------------------------------------------------------
@@ -54,5 +44,20 @@ class TaleRepository
         .includes(:tags)
         .includes(:sequels)
         .first
+  end
+
+  # -----------------------------------------------------------------
+  # Support
+  # -----------------------------------------------------------------
+  class << self
+    private
+    # common logic for Read - index
+    def read_index(condition, page)
+      condition
+          .page(page)
+          .per(RECORD_LIST_LIMIT_SIZE)
+          .order(created_at: :desc)
+          .includes(:tale_tag_relationships)
+    end
   end
 end
