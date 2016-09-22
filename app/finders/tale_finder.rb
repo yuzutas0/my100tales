86cfcff5587ac985d23b53e1590b9540192cf898
yuzutas0 <sho.y0921@gmail.com>
2016-09-22 20:18:44 +0900

    translate logic about finding-record from repository-class to finder-module

--- app/finders/tale_finder.rb
+++ app/finders/tale_finder.rb
@@ -2,27 +2,89 @@
 module TaleFinder
   extend ActiveSupport::Concern
 
-  # methods
+  # -----------------------------------------------------------------
+  # Const
+  # -----------------------------------------------------------------
+  LIMIT_SIZE = 10.freeze
+  QUERY = {
+      user: 'tales.user_id = ?',
+      tags: 'tale_tag_relationships.tag_id IN (?)',
+      keyword: '(tales.title LIKE ? OR tales.content LIKE ?)',
+      and: ' AND '
+  }.freeze
+
+  # -----------------------------------------------------------------
+  # Methods
+  # -----------------------------------------------------------------
   module ClassMethods
-    # search
+
+    # called by TaleRepository#list
+    def index_by_db(user_id, tags, page)
+      query = QUERY[:user]
+      condition = if tags.present?
+                    query = query + QUERY[:and] + QUERY[:tags]
+                    Tale.joins(:tale_tag_relationships)
+                        .where(query, user_id, tags)
+                  else
+                    Tale.where(query, user_id)
+                  end
+      read(condition, page)
+    end
+
+    # called by TaleRepository#search_by_db
+    def search_by_db(user_id, keyword, tags, page)
+      keyword = '%' + keyword + '%'
+      query = QUERY[:user] + QUERY[:and] + QUERY[:keyword]
+      condition = if tags.present?
+                    query = query + QUERY[:and] + QUERY[:tags]
+                    Tale.joins(:tale_tag_relationships)
+                        .where(query, user_id, keyword, keyword, tags)
+                  else
+                    Tale.where(query, user_id, keyword, keyword)
+                  end
+      read(condition, page)
+    end
+
+    # called by TaleRepository#search_by_es
+    def search_by_es(user_id, keyword, tags, page)
+      condition = Tale.search_request(user_id, keyword).records
+      read(condition, page)
+    end
+
+    # -----------------------------------------------------------------
+    # Support
+    # -----------------------------------------------------------------
+    private
+
+    # common logic
+    def read(condition, page)
+      condition
+          .uniq
+          .page(page)
+          .per(LIMIT_SIZE)
+          .order(created_at: :desc)
+          .includes(:tale_tag_relationships)
+    end
+
+    # keyword search by elasticsearch
     # FIXME: kaminari, analyzer
-    def search_index(user_id, keyword)
+    def search_request(user_id, keyword)
       __elasticsearch__.search(
-        query: {
-          bool: {
-            should: [
-              { term: { title: keyword.downcase } },
-              { term: { content: keyword.downcase } }
-            ]
-          }
-        },
-        filter: {
-          term: { user_id: user_id }
-        },
-        sort: [
-          { created_at: { order: 'desc' } }
-        ],
-        size: 10_000
+          query: {
+              bool: {
+                  should: [
+                      { term: { title: keyword.downcase } },
+                      { term: { content: keyword.downcase } }
+                  ]
+              }
+          },
+          filter: {
+              term: { user_id: user_id }
+          },
+          sort: [
+              { created_at: { order: 'desc' } }
+          ],
+          size: 10_000
       )
     end
   end
--- app/repositories/tale_repository.rb
+++ app/repositories/tale_repository.rb
@@ -4,46 +4,19 @@ class TaleRepository
   # Read - index
   # -----------------------------------------------------------------
 
-  # const
-  READ_INDEX_LIMIT_SIZE = 10.freeze
-  READ_INDEX_QUERY = {
-      user: 'tales.user_id = ?',
-      tags: 'tale_tag_relationships.tag_id IN (?)',
-      keyword: '(tales.title LIKE ? OR tales.content LIKE ?)',
-      and: ' AND '
-  }.freeze
-
   # get index by MariaDB
   def self.list(user_id, tags, page)
-    query = READ_INDEX_QUERY[:user]
-    condition = if tags.present?
-                  query = query + READ_INDEX_QUERY[:and] + READ_INDEX_QUERY[:tags]
-                  Tale.joins(:tale_tag_relationships)
-                      .where(query, user_id, tags)
-                else
-                  Tale.where(query, user_id)
-                end
-    read_index(condition, page)
+    Tale.index_by_db(user_id, tags, page)
   end
 
   # search by MariaDB
   def self.search_by_db(user_id, keyword, tags, page)
-    keyword = '%' + keyword + '%'
-    query = READ_INDEX_QUERY[:user] + READ_INDEX_QUERY[:and] + READ_INDEX_QUERY[:keyword]
-    condition = if tags.present?
-                  query = query + READ_INDEX_QUERY[:and] + READ_INDEX_QUERY[:tags]
-                  Tale.joins(:tale_tag_relationships)
-                      .where(query, user_id, keyword, keyword, tags)
-                else
-                  Tale.where(query, user_id, keyword, keyword)
-                end
-    read_index(condition, page)
+    Tale.search_by_db(user_id, keyword, tags, page)
   end
 
   # search by Elasticsearch
   def self.search_by_es(user_id, keyword, tags, page)
-    condition = Tale.search_index(user_id, keyword).records
-    read_index(condition, page)
+    Tale.search_by_es(user_id, keyword, tags, page)
   end
 
   # -----------------------------------------------------------------
@@ -62,21 +35,5 @@ class TaleRepository
         .includes(:tags)
         .includes(:sequels)
         .first
-  end
-
-  # -----------------------------------------------------------------
-  # Support
-  # -----------------------------------------------------------------
-  class << self
-    private
-    # common logic for Read - index
-    def read_index(condition, page)
-      condition
-          .uniq
-          .page(page)
-          .per(READ_INDEX_LIMIT_SIZE)
-          .order(created_at: :desc)
-          .includes(:tale_tag_relationships)
-    end
   end
 end
