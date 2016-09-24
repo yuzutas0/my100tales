39e81bd9d934f1b33c740aeee4522eaa3338973e
yuzutas0 <sho.y0921@gmail.com>
2016-09-24 20:25:06 +0900

    enable html-rendered-page to get each-page-title

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,9 @@
 # ApplicationHelper
 #
 module ApplicationHelper
+  def page_title
+    title = Constants::PRODUCT_NAME
+    title = @page_title + ' - ' + title if @page_title
+    title
+  end
 end
--- config/initializers/constants.rb
+++ config/initializers/constants.rb
@@ -1,12 +1,23 @@
 # manage constant value
 module Constants
+  # product name
+  PRODUCT_NAME = 'my100tales'.freeze
+
   # routes name to show user
   HOME_INDEX_PATH_NAME = 'Top'.freeze
+
+  LOG_IN_PATH_NAME = 'LogIn'.freeze
+  SIGN_UP_PATH_NAME = 'SignUp'.freeze
+  UNLOCK_PATH_NAME = 'Unlock'.freeze
+  ACCOUNT_PATH_NAME = 'Account'.freeze
+  PASSWORD_PATH_NAME = 'Password'.freeze
+
   TALES_PATH_NAME = 'MyPage'.freeze
   SHOW_TALE_PATH_NAME = 'YourTale'.freeze
-  NEW_TALE_PATH_NAME = 'New Tale'.freeze
-  EDIT_TALE_PATH_NAME = 'Edit Tale'.freeze
-  TAG_INDEX_PATH_NAME = 'Tag List'.freeze
+  NEW_TALE_PATH_NAME = 'NewTale'.freeze
+  EDIT_TALE_PATH_NAME = 'EditTale'.freeze
+
+  TAG_INDEX_PATH_NAME = 'TagList'.freeze
 
   # message
   SEQUEL_NOT_EXISTS = 'There is no sequel.'.freeze
