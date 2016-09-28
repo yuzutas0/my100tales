2c89f87e4c866f2a8f1de3ed57e2b5eeb1cce856
yuzutas0 <sho.y0921@gmail.com>
2016-09-28 23:40:01 +0900

    refactor

--- app/models/search_condition.rb
+++ app/models/search_condition.rb
@@ -11,7 +11,7 @@ class SearchCondition < ActiveRecord::Base
   # validation
   # -----------------------------------------------------------------
   validates :name, length: { minimum: 1, maximum: 255 }
-  validates :query_string, length: { minimum: 1, maximum: 1_500 }, presence: true
+  validates :query_string, presence: true, length: { minimum: 1, maximum: 1_500 }
   validates :save_flag, inclusion: { in: [true, false] }
   validates :user, presence: true
 end
