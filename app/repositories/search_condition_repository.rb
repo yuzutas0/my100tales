69fea16ad12956e76bee6c1db622c8d1fdcb8904
yuzutas0 <sho.y0921@gmail.com>
2016-09-29 01:43:53 +0900

    implement search condition logic

--- app/repositories/search_condition_repository.rb
+++ app/repositories/search_condition_repository.rb
@@ -1,6 +1,11 @@
 # search_condition_repository
 class SearchConditionRepository
   # -----------------------------------------------------------------
+  # Const
+  # -----------------------------------------------------------------
+  RECORD_SIZE = 5
+
+  # -----------------------------------------------------------------
   # Read
   # -----------------------------------------------------------------
   def self.select_by_user(user_id)
@@ -10,8 +15,35 @@ class SearchConditionRepository
   # -----------------------------------------------------------------
   # Update
   # -----------------------------------------------------------------
-  def update_time(search_condition)
+  def self.update_time(search_condition)
     search_condition.updated_at = Time.now
     search_condition.update
+  end
+
+  # -----------------------------------------------------------------
+  # Delete
+  # -----------------------------------------------------------------
+  # the records count = 5 after this query
+  def self.delete_to_size(user_id, save_flag, present_size)
+    # validate
+    delete_size = present_size - RECORD_SIZE
+    return unless delete_size > 0
+    # query
+    query = <<-'SQL'.freeze
+      DELETE
+      FROM
+        search_conditions S
+      WHERE
+        S.user_id = ?
+        AND S.save_flag = ?
+      ORDER BY
+        S.updated_at ASC
+      LIMIT
+        ?
+    SQL
+    # execute
+    args = [query, user_id, save_flag, delete_size]
+    sql = ActiveRecord::Base.send(:sanitize_sql_array, args)
+    ActiveRecord::Base.connection.execute(sql)
   end
 end
--- app/services/search_condition_service.rb
+++ app/services/search_condition_service.rb
@@ -10,23 +10,20 @@ class SearchConditionService
     present_list = SearchConditionRepository.select_by_user(user.id)
     return present_list if query_string.blank?
 
-    # -> if query has been not already saved (save_flag == false)
-    #   -> create new history
-    SearchConditionFactory.create(user, query_string, false, name)
-    #   -> if the history record size is over five
-    #      -> delete history to make size five (wanna delete oldest records)
-
-    # -> if query has been already saved
-    #   -> update the record's time
-    search_condition = SearchCondition.new # FIXME
-    SearchConditionRepository.update_time(search_condition)
+    # save_flag == false && query_string == #{query_string}
+    to_be_updated = (present_list.select { |item| !item.save_flag && item.query_string == query_string }).first
+    if to_be_updated.present?
+      SearchConditionRepository.update_time(to_be_updated)
+    else
+      SearchConditionFactory.create(user, query_string, false, name)
+      present_size = present_list.reject(&:save_flag).length + 1
+      SearchConditionRepository.delete_to_size(user.id, save_flag, present_size)
+    end
 
     if save_flag
-    # if param exists && save_flag == true
-    #   -> create new save
       SearchConditionFactory.create(user, query_string, true, name)
-    #   -> if the record size is over five
-    #     -> delete record to make size five (wanna delete oldest records)
+      present_size = present_list.select(&:save_flag).length + 1
+      SearchConditionRepository.delete_to_size(user.id, save_flag, present_size)
     end
 
     # return all records
