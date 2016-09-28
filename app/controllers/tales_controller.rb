b58107b3c0f010e477973f74a876dc983875d0dd
yuzutas0 <sho.y0921@gmail.com>
2016-09-29 02:23:03 +0900

    enable search-form to treat original-request-query-string

--- app/controllers/tales_controller.rb
+++ app/controllers/tales_controller.rb
@@ -33,7 +33,7 @@ class TalesController < ApplicationController
   # -----------------------------------------------------------------
   # GET /tales
   def index
-    @queries = SearchForm.new(params)
+    @queries = SearchForm.new(params, request.fullpath)
     @is_searched, @tales, @tags, @tags_attached, @sequels_attached = TaleService.list(current_user.id, @queries)
     @sort_master = SearchForm.sort_master
   end
--- app/forms/search_form.rb
+++ app/forms/search_form.rb
@@ -1,17 +1,18 @@
 # search form
 class SearchForm
-  attr_accessor :page, :keyword, :tags, :sort, :save, :name
+  attr_accessor :page, :keyword, :tags, :sort, :save, :name, :query_string
 
   # -----------------------------------------------------------------
   # Constructor
   # -----------------------------------------------------------------
-  def initialize(params = {})
+  def initialize(params = {}, request_path = '')
     @page = params[:page] || 1
     @keyword = params[:keyword].html_safe if params[:keyword].present?
     @tags = valid_tags?(params[:tags]) ? convert_tags(params[:tags]) : []
     @sort = valid_sort?(params[:sort]) ? params[:sort].to_i : 0
     @save = params[:save].is_a?(TrueClass)
     @name = params[:name].html_safe if params[:name].present?
+    @query_string = request_path.include?('?') ? request_path.html_safe.split('?')[1] : ''
   end
 
   # -----------------------------------------------------------------
