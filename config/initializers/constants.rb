8eabc385871fb072c278e4749b2201f9f516fb51
yuzutas0 <sho.y0921@gmail.com>
2016-10-08 17:04:24 +0900

    trasfer view-message from const to locales

--- config/initializers/constants.rb
+++ config/initializers/constants.rb
@@ -3,11 +3,6 @@ module Constants
   # product name
   PRODUCT_NAME = 'my100tales'.freeze
 
-  # message
-  SEQUEL_NOT_EXISTS = 'There is no sequel.'.freeze
-  TAG_NOT_EXISTS = 'There is no tag.'.freeze
-  SEARCH_CONDITION_NOT_EXISTS = 'There is no search'.freeze
-
   # magic number
   SEARCH_CONDITION_RECORD_SIZE = 10
 end
