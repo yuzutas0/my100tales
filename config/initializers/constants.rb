5c286ecbfbe8e979b89b4ae94c90a4d17a6248ad
yuzutas0 <sho.y0921@gmail.com>
2016-10-08 16:50:34 +0900

    trasfer view-text from const to locales

--- config/initializers/constants.rb
+++ config/initializers/constants.rb
@@ -4,21 +4,6 @@ module Constants
   PRODUCT_NAME = 'my100tales'.freeze
 
   # routes name to show user
-  HOME_INDEX_PATH_NAME = 'Top'.freeze
-
-  LOG_IN_PATH_NAME = 'LogIn'.freeze
-  SIGN_UP_PATH_NAME = 'SignUp'.freeze
-  UNLOCK_PATH_NAME = 'Unlock'.freeze
-  ACCOUNT_PATH_NAME = 'Account'.freeze
-  PASSWORD_PATH_NAME = 'Password'.freeze
-
-  TALES_PATH_NAME = 'MyPage'.freeze
-  SHOW_TALE_PATH_NAME = 'YourTale'.freeze
-  NEW_TALE_PATH_NAME = 'NewTale'.freeze
-  EDIT_TALE_PATH_NAME = 'EditTale'.freeze
-
-  TAG_INDEX_PATH_NAME = 'TagList'.freeze
-
   CONDITIONS_INDEX_PATH_NAME = 'SearchConditionList'.freeze
   EDIT_CONDITIONS_PATH_NAME = 'Edit List'.freeze
 
