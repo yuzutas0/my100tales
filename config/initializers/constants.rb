6c850781118cfe588765750a978e8b4190d871c8
yuzutas0 <sho.y0921@gmail.com>
2016-10-08 16:55:00 +0900

    trasfer view-text from const to locales

--- config/initializers/constants.rb
+++ config/initializers/constants.rb
@@ -3,10 +3,6 @@ module Constants
   # product name
   PRODUCT_NAME = 'my100tales'.freeze
 
-  # routes name to show user
-  CONDITIONS_INDEX_PATH_NAME = 'SearchConditionList'.freeze
-  EDIT_CONDITIONS_PATH_NAME = 'Edit List'.freeze
-
   # message
   SEQUEL_NOT_EXISTS = 'There is no sequel.'.freeze
   TAG_NOT_EXISTS = 'There is no tag.'.freeze
