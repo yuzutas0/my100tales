ed13c9d4acd1b0ba40c38ee23d71d0539d6813e7
yuzutas0 <sho.y0921@gmail.com>
2016-10-08 18:31:27 +0900

    devide i18n text into some files

--- config/application.rb
+++ config/application.rb
@@ -28,7 +28,7 @@ module My100tales
     # config.time_zone = 'Central Time (US & Canada)'
 
     # locales
-    # config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}').to_s]
+    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}').to_s]
     config.i18n.available_locales = [:en, :ja]
     config.i18n.default_locale = :ja
 
