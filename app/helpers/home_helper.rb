cb1162f2858ca6b919ae36812ad1e02eaba8e9c3
yuzutas0 <sho.y0921@gmail.com>
2017-01-04 00:28:42 +0900

    fix style layout about privacy

--- app/helpers/home_helper.rb
+++ app/helpers/home_helper.rb
@@ -2,13 +2,17 @@
 # HomeHelper
 #
 module HomeHelper
-  def render_i18n(html_text)
-    raw(html_text
+  def i18n_text_format(html_text)
+    raw '<p>' + html_text.gsub(/(\d+\..+)/, '<b>\1</b>').gsub(/\r\n|\r|\n/, '<br />') + '</p>'
+  end
+
+  def i18n_table_format(html_text)
+    raw html_text
             .gsub(/>(\r\n|\r|\n)/, '>')
             .gsub(/<th>/, '<th><p>')
             .gsub(/<\/th>/, '<p></th>')
             .gsub(/<td>/, '<td><p>')
             .gsub(/<\/td>/, '<p></td>')
-            .gsub(/\r\n|\r|\n/, '<br />'))
+            .gsub(/\r\n|\r|\n/, '<br />')
   end
 end
