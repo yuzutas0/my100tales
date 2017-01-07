ee9254e59303f006d90e62f0c818ce39e2a6ca28
yuzutas0 <sho.y0921@gmail.com>
2017-01-07 23:46:56 +0900

    add config about unicorn

--- config/unicorn/production.rb
+++ config/unicorn/production.rb
@@ -6,14 +6,26 @@ preload_app true
 listen '/var/run/nginx-rails.sock' # listen '/tmp/unicorn.sock'
 pid '/tmp/unicorn.pid'
 
-before_fork do |_server, _worker|
+before_fork do |server, worker|
   Signal.trap 'TERM' do
     puts 'Unicorn master intercepting TERM and sending myself QUIT instead'
     Process.kill 'QUIT', Process.pid
   end
 
-  defined?(ActiveRecord::Base) and
-      ActiveRecord::Base.connection.disconnect!
+  ActiveRecord::Base.connection.disconnect! if defined?(ActiveRecord::Base)
+  # TODO: Redis
+  # TODO: Elasticsearch
+
+  old_pid = "#{server.config[:pid]}.oldbin"
+  if old_pid != server.pid
+    begin
+      sig = (worker.nr + 1) >= server.worker_processes ? :QUIT : :TTOU
+      Process.kill(sig, File.read(old_pid).to_i)
+    rescue Errno::ENOENT, Errno::ESRCH
+    end
+  end
+
+  sleep 1
 end
 
 after_fork do |_server, _worker|
@@ -21,8 +33,9 @@ after_fork do |_server, _worker|
     puts 'Unicorn worker intercepting TERM and doing nothing. Wait for master to send QUIT'
   end
 
-  defined?(ActiveRecord::Base) and
-      ActiveRecord::Base.establish_connection
+  ActiveRecord::Base.establish_connection if defined?(ActiveRecord::Base)
+  # TODO: Redis
+  # TODO: Elasticsearch
 end
 
 stderr_path File.expand_path('log/unicorn.log', ENV['RAILS_ROOT'])
