06311f0906f12bd587b95dbf67991cd61211bc83
yuzutas0 <sho.y0921@gmail.com>
2017-01-07 22:41:48 +0900

    add manual to install mariadb

--- server/production/middleware/1_mariadb.sh
+++ server/production/middleware/1_mariadb.sh
@@ -16,8 +16,33 @@ systemctl enable mariadb.service
 mysql_secure_installation
 # current password => N/A (only push enter key)
 # set password => Y
-# new password & confirm password => [new password](Todo: memorialize)
+# new password & confirm password => [root password]
 # remove anonymous => Y
 # disallow remote root login => Y
 # remove test => Y
 # reload => Y
+
+mysql -u root -p
+# enter [root password]
+
+# create user [user];
+# set password for [user] = password('[user password]');
+# create database [schema];
+# grant all on [schema].* to [user];
+
+# show databases
+# select User from mysql.user;
+# exit
+
+mysql -u [user] -p
+# enter [user password]
+
+# show databases;
+# use [scheme];
+# show variables like "chara%";
+#   => make sure there's not 'utf8'.
+# exit
+
+# todo: backup monthly
+# mysqldump --single-transaction -u root -p -x -all-database > [backup file]
+# enter [root password]
