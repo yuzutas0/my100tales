fd9e65541bfeadcba1c3aa218b61dc01a998d630
yuzutas0 <sho.y0921@gmail.com>
2017-01-07 23:13:32 +0900

    formatter

--- config/unicorn/production.rb
+++ config/unicorn/production.rb
@@ -6,7 +6,7 @@ preload_app true
 listen '/tmp/unicorn.sock'
 pid '/tmp/unicorn.pid'
 
-before_fork do |server, worker|
+before_fork do |_server, _worker|
   Signal.trap 'TERM' do
     puts 'Unicorn master intercepting TERM and sending myself QUIT instead'
     Process.kill 'QUIT', Process.pid
@@ -16,7 +16,7 @@ before_fork do |server, worker|
       ActiveRecord::Base.connection.disconnect!
 end
 
-after_fork do |server, worker|
+after_fork do |_server, _worker|
   Signal.trap 'TERM' do
     puts 'Unicorn worker intercepting TERM and doing nothing. Wait for master to send QUIT'
   end
