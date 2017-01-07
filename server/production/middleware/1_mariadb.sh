ad0071346e1470bff73b7a6fad39010f4288582f
yuzutas0 <sho.y0921@gmail.com>
2017-01-07 22:26:11 +0900

    add manual to install mariadb

--- server/production/middleware/1_mariadb.sh
+++ server/production/middleware/1_mariadb.sh
@@ -1,1 +1,20 @@
 #!/bin/bash
+
+su
+
+yum install mariadb mariadb-server
+
+vi /etc/my.conf
+# character-set-server=utf8mb4
+
+systemctl start mariadb.service
+systemctl enable mariadb.service
+
+mysql_secure_installation
+# current password => N/A (only push enter key)
+# set password => Y
+# new password & confirm password => [new password](Todo: memorialize)
+# remove anonymous => Y
+# disallow remote root login => Y
+# remove test => Y
+# reload => Y
