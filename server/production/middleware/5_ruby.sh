c8100a32fd1bd6b051cee4d9845c7f063ed38d81
yuzutas0 <sho.y0921@gmail.com>
2017-01-08 19:12:30 +0900

    add manual to install ruby

--- server/production/middleware/5_ruby.sh
+++ server/production/middleware/5_ruby.sh
@@ -1,1 +1,24 @@
 #!/bin/bash
+
+# [ruby version] = 2.3.0
+
+ruby -v
+sudo yum remove ruby # if ruby has already installed
+
+sudo yum install git
+git clone https://github.com/sstephenson/rbenv.git ~/.rbenv
+echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> ~/.bash_profile
+echo 'eval "$(rbenv init -)"' >> ~/.bash_profile
+source ~/.bash_profile
+
+git clone git://github.com/sstephenson/ruby-build.git ~/.rbenv/plugins/ruby-build
+sudo ~/.rbenv/plugins/ruby-build/install.sh
+rbenv -v
+
+rbenv install [ruby version]
+rbenv rehash
+rbenv global [ruby version]
+
+ruby -v
+which ruby
+which gem
