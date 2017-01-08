607297e6b770c2b3fc050543b11fc0f99385defb
yuzutas0 <sho.y0921@gmail.com>
2017-01-08 19:04:28 +0900

    add manual to install elasticsearch

--- server/production/middleware/3_elasticsearch.sh
+++ server/production/middleware/3_elasticsearch.sh
@@ -1,1 +1,22 @@
 #!/bin/bash
+
+# refs. https://www.elastic.co/guide/en/elasticsearch/reference/current/rpm.html
+
+rpm --import https://packages.elastic.co/GPG-KEY-elasticsearch
+
+vi /etc/yum.repos.d/elasticsearch.repo
+
+# [elasticsearch-2.x]
+# name=Elasticsearch repository for 2.x packages
+# baseurl=http://packages.elastic.co/elasticsearch/2.3.5/centos
+# gpgcheck=1
+# gpgkey=http://packages.elastic.co/GPG-KEY-elasticsearch
+# enabled=1
+
+yum install elasticsearch
+
+systemctl start elasticsearch.service
+systemctl enable elasticsearch.service
+
+cd /usr/share/elasticsearch
+sudo bin/plugin install analysis-kuromoji
