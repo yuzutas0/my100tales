48008f4200edb07f986910fb8eb43d4d4b1c0eef
yuzutas0 <sho.y0921@gmail.com>
2017-01-08 19:05:48 +0900

    add manual to install redis

--- server/production/middleware/2_redis.sh
+++ server/production/middleware/2_redis.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
+wget http://rpms.famillecollet.com/enterprise/remi-release-7.rpm
+
 su
 
-yum install redis
+yum install epel-release
+rpm -Uvh remi-release-7*.rpm
+yum --enablerepo=remi,remi-test,epel install redis
 
 systemctl start redis.service
 systemctl enable redis.service
-
-# => check by 'redis-cli ping'
