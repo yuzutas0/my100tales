864a08e5f85859a89904874e69899724f8ba5537
yuzutas0 <sho.y0921@gmail.com>
2017-01-08 18:50:29 +0900

    add manual to install redis

--- server/production/middleware/2_redis.sh
+++ server/production/middleware/2_redis.sh
@@ -1,1 +1,11 @@
 #!/bin/bash
+
+wget http://rpms.famillecollet.com/enterprise/remi-release-7.rpm
+
+su
+
+yum install epel-release
+rpm -Uvh remi-release-7*.rpm
+
+yum --enablerepo=remi,remi-test,epel install redis
+
