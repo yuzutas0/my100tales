7f5237d037778e4081039f27bc0067681bab46ae
yuzutas0 <sho.y0921@gmail.com>
2017-01-08 19:22:47 +0900

    add manual to install rails

--- server/production/middleware/5_ruby.sh
+++ server/production/middleware/5_ruby.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 
 # [ruby version] = 2.3.0
+# [domain] = yuzutas0.com
 
 ruby -v
 sudo yum remove ruby # if ruby has already installed
 
 sudo yum install git
+git config --global user.name "[admin]"
+git config --global user.email "[admin@" + [domain] + "]"
+
 git clone https://github.com/sstephenson/rbenv.git ~/.rbenv
 echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> ~/.bash_profile
 echo 'eval "$(rbenv init -)"' >> ~/.bash_profile
--- server/production/middleware/6_rails.sh
+++ server/production/middleware/6_rails.sh
@@ -1,1 +1,13 @@
 #!/bin/bash
+
+# [application name] = my100tales
+# [repository] = https://github.com/yuzutas0/my100tales.git
+
+rbenv exec gem install bundler
+rbenv rehash
+
+cd /var/www/
+git clone [repository]
+cd [application name]
+
+bundle install --path vendor/bundle
