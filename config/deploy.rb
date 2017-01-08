66ad2d2e3bf5e38cd97b20703fbb33822b0cebd1
yuzutas0 <sho.y0921@gmail.com>
2017-01-08 19:57:18 +0900

    add settings about Capistrano

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,8 +1,8 @@
 # config valid only for current version of Capistrano
-lock "3.7.1"
+lock '3.7.1'
 
-set :application, "my_app_name"
-set :repo_url, "git@example.com:me/my_repo.git"
+set :application, 'my100tales'
+set :repo_url, 'https://github.com/yuzutas0/my100tales.git'
 
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
