da75206d7be178632edd27bf3343a70cf422b7ec
yuzutas0 <yuzutas0@users.noreply.github.com>
2017-01-15 21:53:38 +0900

    add shell script to set antivirus software

--- server/production/centos/6_virus.sh
+++ server/production/centos/6_virus.sh
@@ -1,1 +1,44 @@
 #!/bin/bash
+
+# ================================
+# install
+# ================================
+
+su
+
+yum install clamav clamav-update
+
+vim /etc/freshclam.conf
+# -----------------------------------------
+# from: Example
+# to:   #Example
+# -----------------------------------------
+# from: DatabaseMirror database.clamav.net
+# to:   DatabaseMirror db.jp.clamav.net
+# -----------------------------------------
+
+# ================================
+# daily update
+# ================================
+
+freshclam
+# check whether database is updated.
+
+vim /etc/sysconfig/freshclam
+# -----------------------------------------
+# from: FRESHCLAM_DELAY=disabled.warn
+# to:   #FRESHCLAM_DELAY=disabled.warn
+# -----------------------------------------
+
+# ================================
+# daily execute
+# ================================
+
+mkdir /var/infected_virus
+
+clamscan -r -i --move=/var/infected_virus
+# be sure that there is no virus file
+
+echo "/usr/bin/clamscan -r --quiet --log=/var/log/clamav.log --move=/var/infected_virus" > /etc/cron.daily/clamav
+chmod +x /etc/cron.daily/clamav
+
