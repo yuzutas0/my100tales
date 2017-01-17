8e07fa45d1ecb2ffb31e03568598a99bebcf2609
yuzutas0 <yuzutas0@users.noreply.github.com>
2017-01-18 01:19:59 +0900

    add comment

--- server/production/middleware/5_ruby.sh
+++ server/production/middleware/5_ruby.sh
@@ -1,14 +1,27 @@
 #!/bin/bash
 
+# ================================
+# variables
+# ================================
+
+admin_name=admin
 ruby_version=2.3.0
 domain=yuzutas0.com
+
+# ================================
+# install git
+# ================================
 
 ruby -v
 sudo yum remove ruby # if ruby has already installed
 
 sudo yum install git
-git config --global user.name "[admin]"
-git config --global user.email "[admin@${domain}]"
+git config --global user.name "${admin_name}"
+git config --global user.email "[${admin_name}@${domain}]"
+
+# ================================
+# install rbenv
+# ================================
 
 git clone https://github.com/sstephenson/rbenv.git ~/.rbenv
 echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> ~/.bash_profile
@@ -18,6 +31,10 @@ source ~/.bash_profile
 git clone git://github.com/sstephenson/ruby-build.git ~/.rbenv/plugins/ruby-build
 sudo ~/.rbenv/plugins/ruby-build/install.sh
 rbenv -v
+
+# ================================
+# install ruby
+# ================================
 
 rbenv install ${ruby_version}
 rbenv rehash
