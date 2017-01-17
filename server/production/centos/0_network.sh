f3ff0312ec5d2baf63572a0a6a8e1b82148e001e
yuzutas0 <yuzutas0@users.noreply.github.com>
2017-01-18 00:47:07 +0900

    add script to login server at first

--- server/production/centos/0_network.sh
+++ server/production/centos/0_network.sh
@@ -1,3 +1,19 @@
 #!/bin/bash
 
 # todo: set host & domain name
+
+
+# ================================
+# variables
+# ================================
+
+# look at mail from hosting service
+server_ip=$1
+origin_password=$2
+
+# ================================
+# first login
+# ================================
+
+ssh root@${server_ip}
+# Enter ${origin_password}
