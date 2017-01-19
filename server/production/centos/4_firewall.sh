a0af1e92933bf098fbdd35824baeb13138bae079
yuzutas0 <yuzutas0@users.noreply.github.com>
2017-01-19 21:21:49 +0900

    refactor

--- server/production/centos/4_firewall.sh
+++ server/production/centos/4_firewall.sh
@@ -46,7 +46,9 @@ sysctl -p
 # Guard Attacks
 # ================================
 
-# Spoofing Private Address
+# Keep Connection
+firewall-cmd --permanent --direct --add-rule ipv4 filter INPUT 0 -m state --state ESTABLISHED,RELATED -j ACCEPT
+firewall-cmd --permanent --direct --add-rule ipv4 filter INPUT 0 -i lo -j ACCEPT
 
 # Fragment Packet Attack
 
@@ -60,69 +62,55 @@ firewall-cmd --permanent --direct --add-rule ipv4 filter INPUT 0 -p tcp -m tcp -
 # Blank Packet
 firewall-cmd --permanent --direct --add-rule ipv4 filter INPUT 0 -p tcp --tcp-flags ALL NONE -j DROP
 
-# SYN Flood
-firewall-cmd --permanent --direct --add-chain ipv4 filter syn-flood
-firewall-cmd --permanent --direct --add-rule ipv4 filter INPUT 100 -i enp0 -p icmp --icmp-type echo-request -j syn-flood
-firewall-cmd --permanent --direct --add-rule ipv4 filter syn-flood 150 -m limit --limit 1/s --limit-burst 4 -j RETURN
-firewall-cmd --permanent --direct --add-rule ipv4 filter syn-flood 151 -j LOG --log-prefix "IPTABLES SYN-FLOOD:"
-firewall-cmd --permanent --direct --add-rule ipv4 filter syn-flood 152 -j DROP
-
-firewall-cmd --permanent --direct --add-rule ipv4 filter INPUT 200 -i enp0 -p tcp ! --syn -m state --state NEW -j LOG --log-prefix "IPTABLES SYN-FLOOD:"
-firewall-cmd --permanent --direct --add-rule ipv4 filter INPUT 201 -p tcp ! --syn -m state --state NEW -j DROP
+# SYN Flood / Ping of Death
+firewall-cmd --permanent --direct --add-rule ipv4 filter INPUT 0 -p icmp --icmp-type echo-request -m limit --limit 1/s --limit-burst 4 -j ACCEPT
+firewall-cmd --permanent --direct --add-rule ipv4 filter INPUT 0 -p tcp ! --syn -m state --state NEW -j DROP
 
 # Stealth Scan
 firewall-cmd --permanent --direct --add-rule ipv4 filter INPUT 0 -p tcp --tcp-flags ALL ALL -j DROP
 
-# Spoofing Broadcast Address
-firewall-cmd --permanent --direct --add-rule ipv4 filter INPUT 0 -m pkttype --pkt-type broadcast -j DROP
-
-# Spoofing Multicast Address
-firewall-cmd --permanent --direct --add-rule ipv4 filter INPUT 0 -m pkttype --pkt-type multicast -j DROP
-
 # TCP Connection Flood
 # HTTP GET Flood
 
-# Ping of Death
-firewall-cmd --permanent --direct --add-chain ipv4 filter ping-death
-firewall-cmd --permanent --direct --add-rule ipv4 filter INPUT 300 -i enp0 -p icmp --icmp-type echo-request -j ping-death
-firewall-cmd --permanent --direct --add-rule ipv4 filter ping-death 350 -m limit --limit 1/s --limit-burst 4 -j RETURN
-firewall-cmd --permanent --direct --add-rule ipv4 filter ping-death 351 -j LOG --log-prefix "IPTABLES PING-DEATH:"
-firewall-cmd --permanent --direct --add-rule ipv4 filter ping-death 352 -j DROP
 
 # Port Scan
-firewall-cmd --permanent --direct --add-chain ipv4 filter port-scan
-firewall-cmd --permanent --direct --add-rule ipv4 filter INPUT 400 -i enp0 -p tcp --tcp-flags SYN,ACK,FIN,RST RST -j port-scan
-firewall-cmd --permanent --direct --add-rule ipv4 filter port-scan 450 -m limit --limit 1/s --limit-burst 4 -j RETURN
-firewall-cmd --permanent --direct --add-rule ipv4 filter port-scan 451 -j LOG --log-prefix "IPTABLES PORT-SCAN:"
-firewall-cmd --permanent --direct --add-rule ipv4 filter port-scan 452 -j DROP
+firewall-cmd --permanent --direct --add-rule ipv4 filter INPUT 0 -p tcp --tcp-flags SYN,ACK,FIN,RST -m state --state NEW -j REJECT --reject-with tcp-reset
 
 # Spoofing
-firewall-cmd --permanent --direct --add-chain ipv4 filter spoofing
-firewall-cmd --permanent --direct --add-rule ipv4 filter INPUT 500 -i enp0 -s 127.0.0.0/8 -j spoofing
-firewall-cmd --permanent --direct --add-rule ipv4 filter INPUT 501 -i enp0 -d 127.0.0.0/8 -j spoofing
-firewall-cmd --permanent --direct --add-rule ipv4 filter INPUT 512 -i enp0 -s 127.0.0.1/8 -j spoofing
-firewall-cmd --permanent --direct --add-rule ipv4 filter INPUT 502 -i enp0 -s 10.0.0.0/8 -j spoofing
-firewall-cmd --permanent --direct --add-rule ipv4 filter INPUT 503 -i enp0 -s 172.16.0.0/12 -j spoofing
-firewall-cmd --permanent --direct --add-rule ipv4 filter INPUT 504 -i enp0 -s 192.168.0.0/16 -j spoofing
-firewall-cmd --permanent --direct --add-rule ipv4 filter INPUT 513 -i enp0 -s 192.168.0.0/24 -j spoofing
-firewall-cmd --permanent --direct --add-rule ipv4 filter INPUT 505 -i enp0 -s 192.0.2.0/24 -j spoofing
-firewall-cmd --permanent --direct --add-rule ipv4 filter INPUT 506 -i enp0 -s 169.254.0.0/16 -j spoofing
-firewall-cmd --permanent --direct --add-rule ipv4 filter INPUT 507 -i enp0 -s 224.0.0.0/4 -j spoofing
-firewall-cmd --permanent --direct --add-rule ipv4 filter INPUT 508 -i enp0 -s 240.0.0.0/5 -j spoofing
-firewall-cmd --permanent --direct --add-rule ipv4 filter INPUT 509 -i enp0 -s 0.0.0.0/8 -j spoofing
-firewall-cmd --permanent --direct --add-rule ipv4 filter INPUT 510 -i enp0 -s 255.255.255.255 -j spoofing
-firewall-cmd --permanent --direct --add-rule ipv4 filter INPUT 511 -i enp0 -s 333.333.333.210 -j spoofing
-firewall-cmd --permanent --direct --add-rule ipv4 filter spoofing 550 -j LOG --log-prefix "IPTABLES SPOOFING:"
-firewall-cmd --permanent --direct --add-rule ipv4 filter spoofing 551 -j DROP
+firewall-cmd --permanent --direct --add-rule ipv4 filter INPUT 0 -i enp0 -s 127.0.0.0/8 -j DROP
+firewall-cmd --permanent --direct --add-rule ipv4 filter INPUT 0 -i enp0 -d 127.0.0.0/8 -j DROP
+firewall-cmd --permanent --direct --add-rule ipv4 filter INPUT 0 -i enp0 -s 127.0.0.1/8 -j DROP
+firewall-cmd --permanent --direct --add-rule ipv4 filter INPUT 0 -i enp0 -d 127.0.0.1/8 -j DROP
+firewall-cmd --permanent --direct --add-rule ipv4 filter INPUT 0 -i enp0 -s 10.0.0.0/8 -j DROP
+firewall-cmd --permanent --direct --add-rule ipv4 filter INPUT 0 -i enp0 -d 10.0.0.0/8 -j DROP
+firewall-cmd --permanent --direct --add-rule ipv4 filter INPUT 0 -i enp0 -s 172.16.0.0/12 -j DROP
+firewall-cmd --permanent --direct --add-rule ipv4 filter INPUT 0 -i enp0 -d 172.16.0.0/12 -j DROP
+firewall-cmd --permanent --direct --add-rule ipv4 filter INPUT 0 -i enp0 -s 192.168.0.0/16 -j DROP
+firewall-cmd --permanent --direct --add-rule ipv4 filter INPUT 0 -i enp0 -d 192.168.0.0/16 -j DROP
+firewall-cmd --permanent --direct --add-rule ipv4 filter INPUT 0 -i enp0 -s 192.168.0.0/24 -j DROP
+firewall-cmd --permanent --direct --add-rule ipv4 filter INPUT 0 -i enp0 -d 192.168.0.0/24 -j DROP
+firewall-cmd --permanent --direct --add-rule ipv4 filter INPUT 0 -i enp0 -s 192.0.2.0/24 -j DROP
+firewall-cmd --permanent --direct --add-rule ipv4 filter INPUT 0 -i enp0 -d 192.0.2.0/24 -j DROP
+firewall-cmd --permanent --direct --add-rule ipv4 filter INPUT 0 -i enp0 -s 169.254.0.0/16 -j DROP
+firewall-cmd --permanent --direct --add-rule ipv4 filter INPUT 0 -i enp0 -d 169.254.0.0/16 -j DROP
+firewall-cmd --permanent --direct --add-rule ipv4 filter INPUT 0 -i enp0 -s 224.0.0.0/4 -j DROP
+firewall-cmd --permanent --direct --add-rule ipv4 filter INPUT 0 -i enp0 -d 224.0.0.0/4 -j DROP
+firewall-cmd --permanent --direct --add-rule ipv4 filter INPUT 0 -i enp0 -s 240.0.0.0/5 -j DROP
+firewall-cmd --permanent --direct --add-rule ipv4 filter INPUT 0 -i enp0 -d 240.0.0.0/5 -j DROP
+firewall-cmd --permanent --direct --add-rule ipv4 filter INPUT 0 -i enp0 -s 0.0.0.0/8 -j DROP
+firewall-cmd --permanent --direct --add-rule ipv4 filter INPUT 0 -i enp0 -d 0.0.0.0/8 -j DROP
+firewall-cmd --permanent --direct --add-rule ipv4 filter INPUT 0 -i enp0 -s 255.255.255.255 -j DROP
+firewall-cmd --permanent --direct --add-rule ipv4 filter INPUT 0 -i enp0 -d 255.255.255.255 -j DROP
+firewall-cmd --permanent --direct --add-rule ipv4 filter INPUT 0 -i enp0 -s 333.333.333.210 -j DROP
+firewall-cmd --permanent --direct --add-rule ipv4 filter INPUT 0 -i enp0 -d 333.333.333.210 -j DROP
+firewall-cmd --permanent --direct --add-rule ipv4 filter INPUT 0 -m pkttype --pkt-type broadcast -j DROP
+firewall-cmd --permanent --direct --add-rule ipv4 filter INPUT 0 -m pkttype --pkt-type multicast -j DROP
 
 # Win Port
-firewall-cmd --permanent --direct --add-chain ipv4 filter win
-firewall-cmd --permanent --direct --add-rule ipv4 filter FORWARD 600 -p tcp -m multiport --sport 135,137,138,139,445 -j win
-firewall-cmd --permanent --direct --add-rule ipv4 filter FORWARD 601 -p udp -m multiport --sport 135,137,138,139,445 -j win
-firewall-cmd --permanent --direct --add-rule ipv4 filter FORWARD 602 -p tcp -m multiport --dport 135,137,138,139,445 -j win
-firewall-cmd --permanent --direct --add-rule ipv4 filter FORWARD 603 -p udp -m multiport --dport 135,137,138,139,445 -j win
-firewall-cmd --permanent --direct --add-rule ipv4 filter win 650 -j LOG --log-prefix "USING WIN PORT:"
-firewall-cmd --permanent --direct --add-rule ipv4 filter win 651 -j DROP
+firewall-cmd --permanent --direct --add-rule ipv4 filter FORWARD 0 -p tcp -m multiport -s 135,137,138,139,445 -j DROP
+firewall-cmd --permanent --direct --add-rule ipv4 filter FORWARD 0 -p udp -m multiport -s 135,137,138,139,445 -j DROP
+firewall-cmd --permanent --direct --add-rule ipv4 filter FORWARD 0 -p tcp -m multiport -d 135,137,138,139,445 -j DROP
+firewall-cmd --permanent --direct --add-rule ipv4 filter FORWARD 0 -p udp -m multiport -d 135,137,138,139,445 -j DROP
 
 # ICMP BLOCK (only accept: echo-request、echo-reply)
 firewall-cmd --permanent --add-icmp-block=destination-unreachable
