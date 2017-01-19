ba91b8be7f948105ec482f8222245bbb40285238
yuzutas0 <yuzutas0@users.noreply.github.com>
2017-01-19 21:57:46 +0900

    refactor

--- server/production/centos/4_firewall.sh
+++ server/production/centos/4_firewall.sh
@@ -58,54 +58,57 @@ firewall-cmd --permanent --direct --add-rule ipv4 filter INPUT 0 -p tcp -d 113 -
 
 # UDP Flood
 
-# ICMP Flood (Ping Flood)
-firewall-cmd --permanent --direct --add-rule ipv4 filter INPUT 0 -p icmp --icmp-type 8 -m length --length :85 -m limit --limit 1/s --limit-burst 4 -j ACCEPT
+# ICMP Flood
 firewall-cmd --permanent --direct --add-rule ipv4 filter INPUT 0 -p tcp -m tcp --tcp-flags RST RST -m limit --limit 2/second --limit-burst 2 -j ACCEPT
 
 # Blank Packet
 firewall-cmd --permanent --direct --add-rule ipv4 filter INPUT 0 -p tcp --tcp-flags ALL NONE -j DROP
 
-# SYN Flood / Ping of Death
-firewall-cmd --permanent --direct --add-rule ipv4 filter INPUT 0 -p icmp --icmp-type echo-request -m limit --limit 1/s --limit-burst 4 -j ACCEPT
+# Ping of Death / Ping Flood
+firewall-cmd --permanent --direct --add-rule ipv4 filter INPUT 0 -p icmp --icmp-type echo-request -m hashlimit --hashlimit 1/s --hashlimit-burst 4 --hashlimit-htable-expire 300000 --hashlimit-mode srcip --hashlimit-name t_PING_OF_DEATH -j ACCEPT
+
+# SYN Flood
 firewall-cmd --permanent --direct --add-rule ipv4 filter INPUT 0 -p tcp ! --syn -m state --state NEW -j DROP
+
+# Port Scan
+firewall-cmd --permanent --direct --add-rule ipv4 filter INPUT 0 -p tcp --tcp-flags SYN,ACK,FIN,RST RST -m state --state NEW -j REJECT --reject-with tcp-reset
 
 # Stealth Scan
 firewall-cmd --permanent --direct --add-rule ipv4 filter INPUT 0 -p tcp --tcp-flags ALL ALL -j DROP
 
 # TCP Connection Flood
-# HTTP GET Flood
 
-
-# Port Scan
-firewall-cmd --permanent --direct --add-rule ipv4 filter INPUT 0 -p tcp --tcp-flags SYN,ACK,FIN,RST -m state --state NEW -j REJECT --reject-with tcp-reset
+# HTTP DoS/DDoS
+firewall-cmd --permanent --direct --add-rule ipv4 filter INPUT 0 tcp -m multiport -d 80 -m hashlimit --hashlimit 1/s --hashlimit-burst 100 --hashlimit-htable-expire 300000 --hashlimit-mode srcip --hashlimit-name t_HTTP_DOS -j ACCEPT
+firewall-cmd --permanent --direct --add-rule ipv4 filter INPUT 0 tcp -m multiport -d 443 -m hashlimit --hashlimit 1/s --hashlimit-burst 100 --hashlimit-htable-expire 300000 --hashlimit-mode srcip --hashlimit-name t_HTTPS_DOS -j ACCEPT
 
 # Spoofing
-firewall-cmd --permanent --direct --add-rule ipv4 filter INPUT 0 -i enp0 -s 127.0.0.0/8 -j DROP
-firewall-cmd --permanent --direct --add-rule ipv4 filter INPUT 0 -i enp0 -d 127.0.0.0/8 -j DROP
-firewall-cmd --permanent --direct --add-rule ipv4 filter INPUT 0 -i enp0 -s 127.0.0.1/8 -j DROP
-firewall-cmd --permanent --direct --add-rule ipv4 filter INPUT 0 -i enp0 -d 127.0.0.1/8 -j DROP
-firewall-cmd --permanent --direct --add-rule ipv4 filter INPUT 0 -i enp0 -s 10.0.0.0/8 -j DROP
-firewall-cmd --permanent --direct --add-rule ipv4 filter INPUT 0 -i enp0 -d 10.0.0.0/8 -j DROP
-firewall-cmd --permanent --direct --add-rule ipv4 filter INPUT 0 -i enp0 -s 172.16.0.0/12 -j DROP
-firewall-cmd --permanent --direct --add-rule ipv4 filter INPUT 0 -i enp0 -d 172.16.0.0/12 -j DROP
-firewall-cmd --permanent --direct --add-rule ipv4 filter INPUT 0 -i enp0 -s 192.168.0.0/16 -j DROP
-firewall-cmd --permanent --direct --add-rule ipv4 filter INPUT 0 -i enp0 -d 192.168.0.0/16 -j DROP
-firewall-cmd --permanent --direct --add-rule ipv4 filter INPUT 0 -i enp0 -s 192.168.0.0/24 -j DROP
-firewall-cmd --permanent --direct --add-rule ipv4 filter INPUT 0 -i enp0 -d 192.168.0.0/24 -j DROP
-firewall-cmd --permanent --direct --add-rule ipv4 filter INPUT 0 -i enp0 -s 192.0.2.0/24 -j DROP
-firewall-cmd --permanent --direct --add-rule ipv4 filter INPUT 0 -i enp0 -d 192.0.2.0/24 -j DROP
-firewall-cmd --permanent --direct --add-rule ipv4 filter INPUT 0 -i enp0 -s 169.254.0.0/16 -j DROP
-firewall-cmd --permanent --direct --add-rule ipv4 filter INPUT 0 -i enp0 -d 169.254.0.0/16 -j DROP
-firewall-cmd --permanent --direct --add-rule ipv4 filter INPUT 0 -i enp0 -s 224.0.0.0/4 -j DROP
-firewall-cmd --permanent --direct --add-rule ipv4 filter INPUT 0 -i enp0 -d 224.0.0.0/4 -j DROP
-firewall-cmd --permanent --direct --add-rule ipv4 filter INPUT 0 -i enp0 -s 240.0.0.0/5 -j DROP
-firewall-cmd --permanent --direct --add-rule ipv4 filter INPUT 0 -i enp0 -d 240.0.0.0/5 -j DROP
-firewall-cmd --permanent --direct --add-rule ipv4 filter INPUT 0 -i enp0 -s 0.0.0.0/8 -j DROP
-firewall-cmd --permanent --direct --add-rule ipv4 filter INPUT 0 -i enp0 -d 0.0.0.0/8 -j DROP
-firewall-cmd --permanent --direct --add-rule ipv4 filter INPUT 0 -i enp0 -s 255.255.255.255 -j DROP
-firewall-cmd --permanent --direct --add-rule ipv4 filter INPUT 0 -i enp0 -d 255.255.255.255 -j DROP
-firewall-cmd --permanent --direct --add-rule ipv4 filter INPUT 0 -i enp0 -s 333.333.333.210 -j DROP
-firewall-cmd --permanent --direct --add-rule ipv4 filter INPUT 0 -i enp0 -d 333.333.333.210 -j DROP
+firewall-cmd --permanent --direct --add-rule ipv4 filter INPUT 0 -s 127.0.0.0/8 -j DROP
+firewall-cmd --permanent --direct --add-rule ipv4 filter INPUT 0 -d 127.0.0.0/8 -j DROP
+firewall-cmd --permanent --direct --add-rule ipv4 filter INPUT 0 -s 127.0.0.1/8 -j DROP
+firewall-cmd --permanent --direct --add-rule ipv4 filter INPUT 0 -d 127.0.0.1/8 -j DROP
+firewall-cmd --permanent --direct --add-rule ipv4 filter INPUT 0 -s 10.0.0.0/8 -j DROP
+firewall-cmd --permanent --direct --add-rule ipv4 filter INPUT 0 -d 10.0.0.0/8 -j DROP
+firewall-cmd --permanent --direct --add-rule ipv4 filter INPUT 0 -s 172.16.0.0/12 -j DROP
+firewall-cmd --permanent --direct --add-rule ipv4 filter INPUT 0 -d 172.16.0.0/12 -j DROP
+firewall-cmd --permanent --direct --add-rule ipv4 filter INPUT 0 -s 192.168.0.0/16 -j DROP
+firewall-cmd --permanent --direct --add-rule ipv4 filter INPUT 0 -d 192.168.0.0/16 -j DROP
+firewall-cmd --permanent --direct --add-rule ipv4 filter INPUT 0 -s 192.168.0.0/24 -j DROP
+firewall-cmd --permanent --direct --add-rule ipv4 filter INPUT 0 -d 192.168.0.0/24 -j DROP
+firewall-cmd --permanent --direct --add-rule ipv4 filter INPUT 0 -s 192.0.2.0/24 -j DROP
+firewall-cmd --permanent --direct --add-rule ipv4 filter INPUT 0 -d 192.0.2.0/24 -j DROP
+firewall-cmd --permanent --direct --add-rule ipv4 filter INPUT 0 -s 169.254.0.0/16 -j DROP
+firewall-cmd --permanent --direct --add-rule ipv4 filter INPUT 0 -d 169.254.0.0/16 -j DROP
+firewall-cmd --permanent --direct --add-rule ipv4 filter INPUT 0 -s 224.0.0.0/4 -j DROP
+firewall-cmd --permanent --direct --add-rule ipv4 filter INPUT 0 -d 224.0.0.0/4 -j DROP
+firewall-cmd --permanent --direct --add-rule ipv4 filter INPUT 0 -s 240.0.0.0/5 -j DROP
+firewall-cmd --permanent --direct --add-rule ipv4 filter INPUT 0 -d 240.0.0.0/5 -j DROP
+firewall-cmd --permanent --direct --add-rule ipv4 filter INPUT 0 -s 0.0.0.0/8 -j DROP
+firewall-cmd --permanent --direct --add-rule ipv4 filter INPUT 0 -d 0.0.0.0/8 -j DROP
+firewall-cmd --permanent --direct --add-rule ipv4 filter INPUT 0 -s 255.255.255.255 -j DROP
+firewall-cmd --permanent --direct --add-rule ipv4 filter INPUT 0 -d 255.255.255.255 -j DROP
+firewall-cmd --permanent --direct --add-rule ipv4 filter INPUT 0 -s 333.333.333.210 -j DROP
+firewall-cmd --permanent --direct --add-rule ipv4 filter INPUT 0 -d 333.333.333.210 -j DROP
 firewall-cmd --permanent --direct --add-rule ipv4 filter INPUT 0 -m pkttype --pkt-type broadcast -j DROP
 firewall-cmd --permanent --direct --add-rule ipv4 filter INPUT 0 -m pkttype --pkt-type multicast -j DROP
 
