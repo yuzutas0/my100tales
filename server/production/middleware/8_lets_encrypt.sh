ed13e8534a9c8b6436631dd81b6f3f68f0dac1c8
yuzutas0 <yuzutas0@users.noreply.github.com>
2017-01-20 23:42:04 +0900

    add script to use lets encrypt

--- server/production/middleware/8_lets_encrypt.sh
+++ server/production/middleware/8_lets_encrypt.sh
@@ -1,1 +1,11 @@
 #!/bin/bash
+
+# ================================
+# install
+# ================================
+
+git clone https://github.com/certbot/certbot
+
+cd certbot/
+
+sudo ./certbot-auto
