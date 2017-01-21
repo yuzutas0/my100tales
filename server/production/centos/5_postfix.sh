75c236368404eab74f09b4015ad4b1aec17a5dc7
yuzutas0 <yuzutas0@users.noreply.github.com>
2017-01-21 20:51:05 +0900

    add script to setup server

--- server/production/centos/5_postfix.sh
+++ server/production/centos/5_postfix.sh
@@ -10,7 +10,7 @@ domain_name=$2
 admin_email=$3
 
 # ================================
-# install & settings
+# install
 # ================================
 
 su
@@ -18,29 +18,40 @@ su
 yum -y install postfix
 
 vim /etc/postfix/main.cf
-# ------------------------------------------------------------------------------------------------------------
+# --------------------------------------------------
 # from: #myhostname = virtual.domain.tld
 # to:   myhostname = ${host_name}.${domain_name}
-# ------------------------------------------------------------------------------------------------------------
+# --------------------------------------------------
 # from: #mydomain = domain.tld
 # to:   mydomain = ${domain_name}
-# ------------------------------------------------------------------------------------------------------------
+# --------------------------------------------------
 # from: #myorigin = $mydomain
 # to:   myorigin = $myhostname
-# ------------------------------------------------------------------------------------------------------------
+# --------------------------------------------------
 # from: inet_protocols = all
 # to:   inet_protocols = ipv4
-# ------------------------------------------------------------------------------------------------------------
+# --------------------------------------------------
 # from: #smtpd_banner = $myhostname ESMTP $mail_name
 # to:   smtpd_banner = $myhostname ESMTP unknown
-# ------------------------------------------------------------------------------------------------------------
+# --------------------------------------------------
 
 systemctl restart postfix
 systemctl enable postfix
 
+date | sendmail ${admin_email}
+# check: admin receives email from server
+
 # ================================
-# test
+# user
 # ================================
 
-date | sendmail ${admin_email}
-# check whether admin receives email from server
+vim /etc/aliases
+# --------------------------------------------------
+# from: #root: marc
+# to:   root: ${admin_email}
+# --------------------------------------------------
+
+newaliases
+
+date | mail root
+# check: admin receives email from server.
--- server/production/centos/7_log.sh
+++ server/production/centos/7_log.sh
@@ -1,25 +1,11 @@
 #!/bin/bash
 
 # ================================
-# variables
-# ================================
-
-admin_mail=$1
-
-# ================================
 # logwatch
 # ================================
 
 yum -y install logwatch
-# check /usr/share/logwatch/default.conf/logwatch.conf
-
-vi /etc/aliases
-# root: ${admin_mail}
-
-newaliases
-
-date | mail root
-# check whether ${admin_mail} receives a mail from this server.
+# check: /usr/share/logwatch/default.conf/logwatch.conf
 
 # ================================
 # disksize
