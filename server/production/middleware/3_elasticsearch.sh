4e7237892a4421e1aa5ef2b114654d26ddbbfc2a
yuzutas0 <yuzutas0@users.noreply.github.com>
2017-01-22 16:32:41 +0900

    add script to setup elasticsearch

--- server/production/middleware/3_elasticsearch.sh
+++ server/production/middleware/3_elasticsearch.sh
@@ -1,22 +1,47 @@
 #!/bin/bash
 
+# -----------------------
+# install java
+# -----------------------
+
+yum -y install java-1.8.0-openjdk-headless
+
+java -version
+# check: java command is enabled
+
+# -----------------------
+# install elasticsearch
+# -----------------------
 # refs. https://www.elastic.co/guide/en/elasticsearch/reference/current/rpm.html
 
 rpm --import https://packages.elastic.co/GPG-KEY-elasticsearch
 
-vi /etc/yum.repos.d/elasticsearch.repo
+cat << _EOF > /etc/yum.repos.d/elasticsearch.repo
+[elasticsearch-2.x]
+name=Elasticsearch repository for 2.x packages
+baseurl=http://packages.elastic.co/elasticsearch/2.x/centos
+gpgcheck=1
+gpgkey=http://packages.elastic.co/GPG-KEY-elasticsearch
+enabled=1
+_EOF
 
-# [elasticsearch-2.x]
-# name=Elasticsearch repository for 2.x packages
-# baseurl=http://packages.elastic.co/elasticsearch/2.3.5/centos
-# gpgcheck=1
-# gpgkey=http://packages.elastic.co/GPG-KEY-elasticsearch
-# enabled=1
+yum -y install elasticsearch
 
-yum install elasticsearch
+systemctl daemon-reload
+systemctl start elasticsearch
+systemctl enable elasticsearch
 
-systemctl start elasticsearch.service
-systemctl enable elasticsearch.service
+systemctl status elasticsearch
+# check: active
+
+# -----------------------
+# install plugin
+# -----------------------
 
 cd /usr/share/elasticsearch
-sudo bin/plugin install analysis-kuromoji
+./bin/plugin install analysis-kuromoji
+systemctl restart elasticsearch
+
+yum -y install jq
+curl -X GET 'http://localhost:9200/_nodes/plugins' | jq
+# check: ... "plugins": [{"name": "analysis-kuromoji" ...
