1af477fb8247742ee33ee37c31246970b26a6238
yuzutas0 <yuzutas0@users.noreply.github.com>
2017-01-24 23:55:58 +0900

    add script to make backup at server

--- server/production/centos/8_backup.sh
+++ server/production/centos/8_backup.sh
@@ -10,3 +10,9 @@ cat << _EOF > /etc/rsync_exclude.lst
 + /home/***
 + /var/www/***
 _EOF
+
+cat << _EOF > /etc/cron.daily/rsync
+/usr/bin/rsync -avz --delete --exclude-from=/etc/rsync_exclude.lst / /var/backup
+_EOF
+
+chmod +x /etc/cron.daily/rsync
