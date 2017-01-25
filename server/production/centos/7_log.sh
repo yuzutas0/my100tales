8f22305181a60eba12866f19af8d3acee52c8d78
yuzutas0 <yuzutas0@users.noreply.github.com>
2017-01-25 21:45:09 +0900

    add script to make log and backup

--- server/production/centos/7_log.sh
+++ server/production/centos/7_log.sh
@@ -4,8 +4,28 @@
 # logwatch
 # ================================
 
+app_name=$1
+os_user=$2
+
 yum -y install logwatch
 # check: /usr/share/logwatch/default.conf/logwatch.conf
+
+# TODO: after deployment
+cat << _EOF > /etc/logrotate.d/unicorn
+/var/www/${app_name}/log/unicorn.log {
+  weekly
+  rotate 4
+  missingok
+  notifempty
+  copytruncate
+  create 0664 wheel ${os_user}
+      minsize 1M
+  lastaction
+    pid=/tmp/unicorn.pid
+    test -s $pid && kill -USR1 "$(cat $pid)"
+  endscript
+}
+_EOF
 
 # ================================
 # cpu
--- server/production/centos/8_backup.sh
+++ server/production/centos/8_backup.sh
@@ -1,20 +1,23 @@
 #!/bin/bash
 
-# TODO
+# TODO: after deployment
+app_name=$1
+
 su
 
 yum install -y rsync
 
-# DB, log, ...
 cat << _EOF > /etc/rsync_exclude.lst
 + /home/***
 + /var/www/***
++ /var/log/nginx/***
++ /var/www/${app_name}/log/unicorn.log
 _EOF
 
 mkdir /var/backup
 
-cat << _EOF > /etc/cron.daily/rsync
+cat << _EOF > /etc/cron.monthly/rsync
 /usr/bin/rsync -avz --delete --exclude-from=/etc/rsync_exclude.lst / /var/backup
 _EOF
 
-chmod +x /etc/cron.daily/rsync
+chmod +x /etc/cron.monthly/rsync
