58f3c90fb051ca73db059fc13ca502ffbaf1c367
yuzutas0 <yuzutas0@users.noreply.github.com>
2017-01-28 22:27:52 +0900

    add settings about deployment

--- config/deploy.rb
+++ config/deploy.rb
@@ -6,6 +6,8 @@ set :repo_url, 'https://github.com/yuzutas0/my100tales.git'
 
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
+set :branch, 'master'
+set :scm, :git
 
 # Default deploy_to directory is /var/www/my_app_name
 # set :deploy_to, "/var/www/my_app_name"
@@ -13,13 +15,16 @@ set :deploy_to, '/var/www/my100tales'
 
 # Default value for :format is :airbrussh.
 # set :format, :airbrussh
+set :format, :airbrussh
 
 # You can configure the Airbrussh format using :format_options.
 # These are the defaults.
 # set :format_options, command_output: true, log_file: "log/capistrano.log", color: :auto, truncate: :auto
+set :format_options, command_output: true, log_file: 'log/capistrano.log', color: :auto, truncate: :auto
 
 # Default value for :pty is false
 # set :pty, true
+set :pty, true
 
 # Default value for :linked_files is []
 # append :linked_files, "config/database.yml", "config/secrets.yml"
