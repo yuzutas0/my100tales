b55f6d32389175f1c54107cf871f5cd0b64a97a9
yuzutas0 <yuzutas0@users.noreply.github.com>
2017-01-29 17:44:52 +0900

    add settings about deployment

--- config/deploy.rb
+++ config/deploy.rb
@@ -45,10 +45,29 @@ set :keep_releases, 5
 set :rbenv_type, :user
 set :rbenv_ruby, '2.3.0'
 
-after 'deploy:publishing', 'deploy:restart'
 namespace :deploy do
   desc 'Restart application'
   task :restart do
     invoke 'unicorn:legacy_restart'
+  end
+  desc 'Create database'
+  task :db_create do
+    on roles(:db) do |_host|
+      with rails_env: fetch(:rails_env) do
+        within current_path do
+          execute :bundle, :exec, :rake, 'db:create'
+          execute :bundle, :exec, :rake, 'elasticsearch:create_index'
+        end
+      end
+    end
+  end
+  after :publishing, :restart
+  after :restart, :clear_cache do
+    on roles(:web), in: :groups, limit: 3, wait: 10 do
+      # Here we can do anything such as:
+      # within release_path do
+      #   execute :rake, 'cache:clear'
+      # end
+    end
   end
 end
