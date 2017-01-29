155782cc4dcac4e499a41d3c11c32bd19b54245c
yuzutas0 <yuzutas0@users.noreply.github.com>
2017-01-29 19:07:09 +0900

    add settings about web server

--- server/production/middleware/6_nginx.sh
+++ server/production/middleware/6_nginx.sh
@@ -16,34 +16,52 @@ su
 yum -y install nginx
 
 systemctl start nginx
+systemctl enable nginx
 
 systemctl status nginx
 # check active
 
 # access by browser at local machine
 open ${root_url}
-
-systemctl stop nginx
-systemctl status nginx
-# check inactive
-
 less /etc/nginx/nginx.conf
 
 vim /etc/nginx/conf.d/${app_name}.conf
-#  set $app my100tales
-#  set $host $app.yuzutas0.com;
-#
-#  user nginx nginx;
-#
-#  events {
-#    worker_processes 4;
-#    worker_connections 1024;
-#    worker_rlimit_nofile 65535;
+#  upstream unicorn_server {
+#    server unix:/tmp/unicorn.sock fail_timeout=0;
 #  }
 #
+#  server {
+#    set $app my100tales;
+#    listen 80;
+#    listen [::]:80;
+#
+#    root /var/www/$app/shared/public;
+#    access_log /var/log/nginx/access.log main;
+#
+#    error_page 404 /404.html;
+#    error_page 422 /422.html;
+#    error_page 400 401 403 404 500 501 502 503 504 /500.html;
+#
+#    gzip on;
+#    gzip_proxied any;
+#    gzip_min_length 1k;
+#    gzip_types text/html text/css text/javascript application/x-javascript;
+#
+#    location / {
+#      try_files $uri $uri/ http://unicorn_server;
+#    }
+#  }
+
+
+
+systemctl reload nginx
+
+# after settings about lets encrypt
+
+rm /etc/nginx/conf.d/${app_name}.conf
+vim /etc/nginx/conf.d/${app_name}.conf
 #  upstream unicorn_server {
-#    server unix:/tmp/unicorn.sock;
-#    fail_timeout=0;
+#    server unix:/tmp/unicorn.sock fail_timeout=0;
 #  }
 #
 #  server {
@@ -53,17 +71,14 @@ vim /etc/nginx/conf.d/${app_name}.conf
 #  }
 #
 #  server {
-#    sever_name $host
+#    set $app my100tales;
 #    listen 443 ssl;
 #    listen [::]:443 ssl;
 #
 #    ssl on;
 #    ssl_protocols TLSv1 TLSv1.1 TLSv1.2;
 #    ssl_prefer_server_ciphers on;
-#    ssl_ciphers ECDHE+RSAGCM:ECDH+AESGCM:
-#                DH+AESGCM:ECDH+AES256:DH+AES256:
-#                ECDH+AES128:DH+AES:
-#                !EXPORT:!DES:!3DES:!MD5:!DSS;
+#    ssl_ciphers ECDHE+RSAGCM:ECDH+AESGCM:DH+AESGCM:ECDH+AES256:DH+AES256:ECDH+AES128:DH+AES:!EXPORT:!DES:!3DES:!MD5:!DSS;
 #
 #    ssl_certificate /etc/letsencrypt/live/$host/cert.pem;
 #    ssl_certificate_key /etc/letsencrypt/live/$host/privkey.pem;
@@ -81,17 +96,8 @@ vim /etc/nginx/conf.d/${app_name}.conf
 #    gzip_types text/html text/css text/javascript application/x-javascript;
 #
 #    location / {
-#      proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
-#      proxy_set_header Host $http_host;
-#      proxy_redirect off;
 #      try_files $uri $uri/ http://unicorn_server;
 #    }
 #  }
-
-systemctl start nginx
-systemctl enable nginx
-
-systemctl status nginx
-# check active
 
 systemctl reload nginx
--- server/production/middleware/7_lets_encrypt.sh
+++ server/production/middleware/7_lets_encrypt.sh
@@ -4,28 +4,35 @@
 # variables
 # ================================
 
-domain=$1
-web_root=/var/www/${domain}/$2
+os_user=$1
+domain=$2
 admin_mail=$3
+web_root=/var/www/${domain}/shared/public
 
 # ================================
 # install
 # ================================
 
+cd /home/${os_user}
 git clone https://github.com/certbot/certbot
 cd certbot/
-sudo ./certbot-auto
+
+su
+./certbot-auto
 
 # ================================
 # execute
 # ================================
 
+# TODO: after deployment
+
 # test
-sudo certbot-auto certonly -m ${admin_mail} --agree-tos --non-interactive $* --webroot -w ${web_root} -d ${domain} --test-cert
+./certbot-auto certonly -m ${admin_mail} --agree-tos --non-interactive $* --webroot -w ${web_root} -d ${domain} --test-cert
 
 # production
-sudo certbot-auto certonly -m ${admin_mail} --agree-tos --non-interactive $* --webroot -w ${web_root} -d ${domain} --force-renewal
+./certbot-auto certonly -m ${admin_mail} --agree-tos --non-interactive $* --webroot -w ${web_root} -d ${domain} --force-renewal
 
 # update automatically
-sudo crontab -e
-# add: 50 3 * * 0 certbot-auto renew --post-hook "systemctl restart nginx" 1 > /dev/null 2 > /dev/null
+cat << _EOF > /etc/cron.weekly/certbot
+/home/${os_user}/bin/certbot-auto renew --post-hook "systemctl restart nginx" 1 > /dev/null 2 > /dev/null
+_EOF
