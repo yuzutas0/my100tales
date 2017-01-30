d13428a7e06f23f6c9561a16ba0618d272c31731
yuzutas0 <yuzutas0@users.noreply.github.com>
2017-01-30 22:52:26 +0900

    fix wrong args

--- config/deploy.rb
+++ config/deploy.rb
@@ -31,11 +31,11 @@ set :pty, true
 
 # Default value for :linked_files is []
 # append :linked_files, "config/database.yml", "config/secrets.yml"
-append :linked_files, %w{.env}
+set :linked_files, %w{.env}
 
 # Default value for linked_dirs is []
 # append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/system"
-append :linked_dirs, %w{log tmp/pids tmp/cache tmp/sockets vendor/bundle vendor/assets public/system public/uploads}
+set :linked_dirs, %w{log tmp/pids tmp/cache tmp/sockets vendor/bundle vendor/assets public/system public/uploads}
 
 # Default value for default_env is {}
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
