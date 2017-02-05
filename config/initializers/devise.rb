ef5cef9685a0436256dfaca2794fd31f0791bca4
yuzutas0 <yuzutas0@users.noreply.github.com>
2017-02-05 17:38:11 +0900

    fix wrong variable

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -13,7 +13,7 @@ Devise.setup do |config|
   # Configure the e-mail address which will be shown in Devise::Mailer,
   # note that it will be overwritten if you use your own mailer class
   # with default "from" parameter.
-  config.mailer_sender = "mailer@#{ENV['HOST']}"
+  config.mailer_sender = "mailer@#{ENV['HOST_URL']}"
 
   # Configure the class responsible to send e-mails.
   # config.mailer = 'Devise::Mailer'
