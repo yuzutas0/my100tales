f547790c5a3407c987f3a87fdf666ee06e439f39
yuzutas0 <yuzutas0@users.noreply.github.com>
2017-02-06 22:24:50 +0900

    fix undefined method error

--- app/decorators/sequel_decorator.rb
+++ app/decorators/sequel_decorator.rb
@@ -1,7 +1,7 @@
 # sequel_decorator
 class SequelDecorator < BaseDecorator
   # add flash message about error reasons
-  def flash(sequel, flash)
+  def self.flash(sequel, flash)
     flash_for_redirect(sequel, flash)
   end
 end
